b4a47eec6758840e291ada92889f3fd00199d797
Kyungmin Choi <kyungmin.official0@gmail.com>
2025-03-16 21:39:00 +0900

    hotfix : vercel deploy plugin

--- v2/svelte.config.js
+++ v2/svelte.config.js
@@ -1,6 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
@@ -14,7 +13,4 @@ export default {
         _ : "src/layouts/default-layout.svelte",
         project : "src/layouts/project-layout.svelte",
       }})],
-    kit: {
-        adapter: adapter({})
-      }
 }
--- v2/vite.config.js
+++ v2/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import routify from '@roxi/routify/vite-plugin'
+import dotenv from 'dotenv'
+import vercel from 'vite-plugin-vercel'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),routify({/* config */})],
+  plugins: [svelte(),routify({/* config */}), vercel()],
 })
